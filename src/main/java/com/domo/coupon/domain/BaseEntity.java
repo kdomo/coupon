03b6e100fa3bfc88d9ef79097a4a7b806f6b6b6f
kdomo <dongho5088@naver.com>
2022-10-28 12:26:34 +0900

    [#3] domain, Controller, Service, Repository 생성

---
+++ src/main/java/com/domo/coupon/domain/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.domo.coupon.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity implements Serializable {
+    private static final long serialVersionUID = -882898014121110887L;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
---
+++ src/main/java/com/domo/coupon/service/CouponService.java
@@ -0,0 +1,11 @@
+package com.domo.coupon.service;
+
+import com.domo.coupon.repository.CouponRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CouponService {
+    private final CouponRepository couponRepository;
+}
